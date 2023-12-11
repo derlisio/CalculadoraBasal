24222083fe8f402697511cbe3e4ddaa9e91a9cb2
derlisio <102877289+derlisio@users.noreply.github.com>
2023-12-11 10:41:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const CALCULAR = document.getElementById('calcular');//boton calcular
+const ERROR = document.getElementById('error');//parrafo de error
+const FLU = document.getElementById('flu');//parrafo flujo
+const MAN = document.getElementById('man');// parrafo mantenimiento
+const DIA = document.getElementById('dia');
+
+CALCULAR.addEventListener('click',calculo)
+
+function calculo(){
+    let peso = document.getElementById('peso').value;
+    if (peso>0){
+        if (peso<=30){
+            ERROR.style.display = 'none'
+            DIA.innerHTML= 'diario: '+ segar(peso) +' cc'
+            DIA.style.display='block'
+            FLU.innerHTML= 'mantenimiento: '+ (segar(peso)/24).toFixed(2) +' cc/hr'
+            FLU.style.display='block'
+            MAN.innerHTML= 'man+man/2: '+ (segar(peso)*1.5) +' cc'
+            MAN.style.display='block'
+            
+    
+        }else{
+            ERROR.style.display = 'none'
+            DIA.innerHTML= 'SC*1500: '+ ((superficie(peso)*1500).toFixed(2)) +' cc'
+            DIA.style.display='block'
+            FLU.innerHTML= 'SC*2000: '+ ((superficie(peso)*2000).toFixed(2)) +' cc/hr'
+            FLU.style.display='block'
+            MAN.style.display = 'none';
+            
+        
+        }
+
+    }else{
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+        DIA.style.display='none'
+    }
+
+
+}
+
+function segar(valor){
+    if (valor>20){
+        return (1500+((valor-20)*20))
+    } else if (valor>10){
+        return (1000+((valor-20)*50))
+    }else{
+        return (valor*100)
+    }
+}
+
+function superficie(valor){
+    return ((valor * 4 ) + 7 ) / (valor + 90)
+}
